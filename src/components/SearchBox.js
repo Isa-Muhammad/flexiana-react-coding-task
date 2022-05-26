7f133f3517394675ef693ae179d469c78b539e95
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2022-05-26 12:17:09 +0100

    Project setup

---
+++ src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SearchBox() {
+  return (
+    <div className="main-section py-24 px-96">
+      <input className="searchBox" type="text" placeholder="Search" />
+    </div>
+  );
+}
